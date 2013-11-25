a76fce8900a5481829c32d79dcb2f80647682fc2
Dmitry Bogordaev <d_bogordaev@ugrasu.ru>
2013-11-25 17:14:53 +0300

    table size edit hint added

--- ui_sulte.h
+++ ui_sulte.h
@@ -1,7 +1,7 @@
 /********************************************************************************
 ** Form generated from reading UI file 'sulte.ui'
 **
-** Created: Mon 25. Nov 17:00:15 2013
+** Created: Mon 25. Nov 17:12:08 2013
 **      by: Qt User Interface Compiler version 4.8.1
 **
 ** WARNING! All changes made in this file will be lost when recompiling UI file!
@@ -101,7 +101,7 @@ public:
         spinBox->setFrame(true);
         spinBox->setButtonSymbols(QAbstractSpinBox::NoButtons);
         spinBox->setMinimum(1);
-        spinBox->setMaximum(20);
+        spinBox->setMaximum(21);
         spinBox->setValue(7);
         lcdNumber = new QLCDNumber(controls);
         lcdNumber->setObjectName(QString::fromUtf8("lcdNumber"));
@@ -155,6 +155,15 @@ public:
     void retranslateUi(QMainWindow *sulteForm)
     {
         sulteForm->setWindowTitle(QApplication::translate("sulteForm", "Sulte", 0, QApplication::UnicodeUTF8));
+#ifndef QT_NO_TOOLTIP
+        spinBox->setToolTip(QApplication::translate("sulteForm", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
+"p, li { white-space: pre-wrap; }\n"
+"</style></head><body style=\" font-family:'MS Shell Dlg 2'; font-size:8.25pt; font-weight:400; font-style:normal;\">\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:8pt;\">rotate the wheel </span></p>\n"
+"<p style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:8pt;\">or type</span></p>\n"
+"<p style=\"-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-size:8pt;\"></p></body></html>", 0, QApplication::UnicodeUTF8));
+#endif // QT_NO_TOOLTIP
         spinBox->setSpecialValueText(QString());
         spinBox->setSuffix(QString());
         startButton->setText(QApplication::translate("sulteForm", "Start", 0, QApplication::UnicodeUTF8));
